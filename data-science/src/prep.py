28d019ef422370bfe14a2eb33e216153c96cc0c8
Manoj Menon <47050165+manojmenon1@users.noreply.github.com>
2025-02-16 12:43:05 -0500

    Updated File

--- data-science/src/prep.py
+++ data-science/src/prep.py
@@ -13,43 +13,57 @@ from sklearn.preprocessing import LabelEncoder
 import mlflow
 import logging
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--data", type=str, help="Path to input data")
-    parser.add_argument("--test_train_ratio", type=float, default=0.2)
-    parser.add_argument("--train_data", type=str, help="Path to save train data")
-    parser.add_argument("--test_data", type=str, help="Path to save test data")
+
+def parse_args():
+    '''Parse input arguments'''
+
+    parser = argparse.ArgumentParser("prep")
+    parser.add_argument("--raw_data", type=str, help="Path to raw data")
+    parser.add_argument("--train_data", type=str, help="Path to train dataset")
+    parser.add_argument("--test_data", type=str, help="Path to test dataset")
+    parser.add_argument("--test_train_ratio", type=float, default=0.2, help="Test-train ratio")
     args = parser.parse_args()
 
-    # Start MLflow Run
-    mlflow.start_run()
+    return args
 
-    # Log arguments
-    logging.info(f"Input data path: {args.data}")
-    logging.info(f"Test-train ratio: {args.test_train_ratio}")
+def main(args):
+    '''Read, split, and save datasets'''
 
     # Reading Data
-    df = pd.read_csv(args.raw_data)
-    # df = pd.read_csv(args.data)
+    df = pd.read_csv(args.raw_data)    
 
     # Encode categorical feature
     le = LabelEncoder()
     df['Segment'] = le.fit_transform(df['Segment'])  
-
+    
     # Split Data into train and test datasets
-    train_df, test_df = train_test_split(df, test_size=args.test_train_ratio, random_state=42)  
+    train_df, test_df = train_test_split(df, test_size=args.test_train_ratio, random_state=42)
 
     # Save train and test data
-    os.makedirs(args.train_data, exist_ok=True)  
-    os.makedirs(args.test_data, exist_ok=True)  
-    train_df.to_csv(os.path.join(args.train_data, "train_data.csv"), index=False)  # Specify the name of the train data file
-    test_df.to_csv(os.path.join(args.test_data, "test_data.csv"), index=False)  # Specify the name of the test data file
+    os.makedirs(args.train_data, exist_ok=True)
+    os.makedirs(args.test_data, exist_ok=True)
+    train_df.to_csv(os.path.join(args.train_data, "train.csv"), index=False)
+    test_df.to_csv(os.path.join(args.test_data, "test.csv"), index=False)
 
-    # log the metrics
-    mlflow.log_metric('train_size', len(train_df))  # Log the train dataset size
-    mlflow.log_metric('test_size', len(test_df))  # Log the test dataset size
-    
-    mlflow.end_run()
+    # Log the metrics
+    mlflow.log_metric('train size', train_df.shape[0])
+    mlflow.log_metric('test size', test_df.shape[0])
 
 if __name__ == "__main__":
-    main()
+    mlflow.start_run()
+
+    # Parse Arguments
+    args = parse_args()
+
+    lines = [
+        f"Raw data path: {args.raw_data}",
+        f"Train dataset output path: {args.train_data}",
+        f"Test dataset path: {args.test_data}",
+        f"Test-train ratio: {args.test_train_ratio}",
+    ]
+
+    for line in lines:
+        print(line)
+    
+    main(args)
+    mlflow.end_run()
